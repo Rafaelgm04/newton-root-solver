df974e1475484ab19cc1acba98dfe93723dc637e
Rafaelgm04 <Rafaelgmbr04@gmail.com>
2026-08-17 21:12:02 -0400

    Refactor Newton struct and update related functions for improved functionality

--- Newton.c
+++ Newton.c
@@ -8,23 +8,24 @@
 
 
 Newton init(){
-    Newton p;
+    Newton p = {0};
 
     //p.deriva       = deriva;
     p.get_poli     = get_poli;
     p.escolhe_x    = escolhe_x;
-    p.erro         = erro;
     p.calcula_zero = calcula_zero;
 
     return p;
 }
 
-double funcao(double *polinomio){
+double funcao(double *polinomio,double x){
 
+    double resu = 0;
     for(int i = 0;i < tam_poli;i++){
-        
-    }
 
+        resu += polinomio[i] * pow(x,i);  
+    }
+    return resu;
 }
 
 void deriva (double *polinomio){
@@ -34,7 +35,7 @@ void deriva (double *polinomio){
         polinomio[i] *= i;
         i++;
     }
-    memmove(polinomio, polinomio + 1,(tam_poli - 1)*sizeof(double));
+    memmove(polinomio, polinomio + 1, (tam_poli - 1) * sizeof(double));
     polinomio[i-1] = 0;
 }
 
@@ -51,40 +52,100 @@ void get_poli (Newton *self, char *polinomio){
             break;
         }
 
-        self->poli[ind++] = (int)numero;
+        self->poli[ind++] = numero;
 
         ptr = fim;
     }
 }
 
-char escolhe_x (Newton *self, int a, int b){
+double escolhe_x (Newton *self){
 
-    double *segunda_der;
-    memcpy(segunda_der,self->poli,tam_poli);
+    double segunda_der[tam_poli];
+    memcpy(segunda_der,self->poli,tam_poli * sizeof(double));
 
     deriva(segunda_der);
     deriva(segunda_der);
 
-
+    if(funcao(self->poli,self->a)*funcao(segunda_der,self->a) > 0)
+        return self->a;
+    else
+        return self->b;
 
 }
-double erro (Newton *self, int *poli){
 
 
-}
-double calcula_zero (Newton *self, int a, int b){
+void calcula_zero (Newton *self){
+    //memset(self->x_n, 0, sizeof(self->x_n));
+    self->x_n[0] = self->escolhe_x(self);
+
+    double derivada[tam_poli];
+    memcpy(derivada,self->poli,tam_poli * sizeof(double));
+    deriva(derivada);
 
 
+    unsigned n = 0;
+    while (n < limete) {
+
+        double fx = funcao(
+            self->poli,
+            self->x_n[n]
+        );
+
+        double dfx = funcao(
+            derivada,
+            self->x_n[n]
+        );
+
+        // evita divisao por zero
+        if (fabs(dfx) < 1e-12) {
+            printf("Derivada igual ou muito proxima de zero!\n");
+            self->n = n;
+            return;
+        }
+
+        self->x_n[n + 1] =
+            self->x_n[n] - fx / dfx;
+
+        printf("x[%u] = %.10f",n + 1,self->x_n[n + 1]);
+
+        
+        printf("   erro |(%f) - (%f)| = %f\n",
+            self->x_n[n + 1],
+            self->x_n[n],
+            fabs(self->x_n[n + 1] - self->x_n[n])
+        );
+
+        if (
+            fabs(self->x_n[n + 1] - self->x_n[n])
+            < self->erro
+        ) {
+            self->n = n + 1;
+            return;
+        }
+
+        n++;
+    }
+
+    printf("O programa atingiu o limite!\n");
+
+    self->n = n;
 }
 
 
 
 int main(){
 
+
     Newton teste;
 
-    init(&teste);
+    teste = init();
     get_poli(&teste,"+10+10+0+28+12");
+    teste.a = -3;
+    teste.b = -2;
+    teste.erro = 0.000001;
+    calcula_zero(&teste);
+    printf("%u zero: %f",teste.n, teste.x_n[teste.n]);
+    /*
     for(unsigned i = 0;i < tam_poli;i++){
         printf("%f,",teste.poli[i]);
 
@@ -96,5 +157,8 @@ int main(){
 
     }
 
+    double resu = funcao(teste.poli,1);
+    printf("\nresu: %f\n",resu);
+    */
     return 0;
 }
--- Newton.h
+++ Newton.h
@@ -1,6 +1,8 @@
 
 
 #define tam_poli 11
+#define limete   100
+#define eps      1e-9
 
 typedef struct Newton Newton;
 
@@ -8,13 +10,15 @@ typedef struct Newton
 {
 
     double poli[tam_poli];
+    double erro;
+    double a,b;
 
-
+    double x_n[limete + 1];
+    unsigned n;
     //int    *(*deriva)(Newton *self, int *polinomio);
-    void   (*get_poli)(Newton *self, char *polinomio);
-    char   (*escolhe_x)(Newton *self, int a, int b); 
-    double (*erro)(Newton *self, int *poli);
-    double (*calcula_zero)(Newton *self, int a, int b);
+    void     (*get_poli)(Newton *self, char *polinomio);
+    double   (*escolhe_x)(Newton *self); 
+    void     (*calcula_zero)(Newton *self);
 
 }Newton;
 
@@ -23,7 +27,6 @@ Newton init();
 
 void   deriva (double *polinomio);
 void   get_poli (Newton *self, char *polinomio);
-char   escolhe_x (Newton *self, int a, int b); 
-double erro (Newton *self, int *poli);
-double calcula_zero (Newton *self, int a, int b);
+double   escolhe_x (Newton *self); 
+void calcula_zero (Newton *self);
 
